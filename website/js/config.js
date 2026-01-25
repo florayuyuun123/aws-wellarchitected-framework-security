20cec3b953ea19a852a3bd346911f9bd4cdd050e
florayuyuun123 <fyuyuun@gmail.com>
2026-01-25 20:13:02 +0400

    added initial AWS architecture diagram

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_CONFIG = {
     // Replace with your ALB DNS name
-    BASE_URL: 'http://aws-sec-pillar-prod-alb-1747156605.us-east-1.elb.amazonaws.com',
+    BASE_URL: 'http://aws-sec-pillar-prod-alb-1264310950.us-east-1.elb.amazonaws.com',
     ENDPOINTS: {
         REGISTER: '/api/companies',
         STATUS: '/api/companies',
